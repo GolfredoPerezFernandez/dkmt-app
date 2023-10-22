565da26bb4aa2ac0f9ca674c30113ee01f025aca
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-10-22 14:45:24 -0400

    asd

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+      <>
+        {/* <h1>RainbowKit Example</h1>
+        <p>Check out the documentation <a href="https://www.rainbowkit.com/docs/introduction">here</a>here.</p> */}
+      </>
+    );
+  }
+  
